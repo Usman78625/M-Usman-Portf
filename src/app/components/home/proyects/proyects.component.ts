8db9b0b38df21bdefc2a35dfbaacb6a9f7c3c2f9
M Usman <um7995367@gmail.com>
2024-10-11 21:02:59 +0500

    first commit

---
+++ src/app/components/home/proyects/proyects.component.ts
@@ -0,0 +1,49 @@
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
+
+@Component({
+  selector: 'app-proyects',
+  templateUrl: './proyects.component.html',
+  styleUrls: ['./proyects.component.scss']
+})
+export class ProyectsComponent implements OnInit {
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    navSpeed: 700,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout:3000
+  }
+  
+
+  @ViewChild('imgContainer') imgContainer: ElementRef;
+
+  screenWidth: number;
+
+    constructor() {
+        this.screenWidth = window.innerWidth;
+    }
+
+    ngOnInit(): void {
+        this.onResize();
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event?) {
+        this.screenWidth = window.innerWidth;
+    }
+debug(){
+
+  this.imgContainer.nativeElement.scroll({
+    top: this.imgContainer.nativeElement.scrollHeight,
+    left: 0,
+    behavior: 'smooth',    
+  });
+}
+
+}
